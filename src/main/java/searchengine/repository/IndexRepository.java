bdcdd6aa2799661d63db7e24d74d18671f9cec14
Benhap1 <bahtiyar.bobokulov1@gmail.com>
2024-05-27 15:52:15 +0300

    11 commit: Lemma process added

--- src/main/java/searchengine/repository/IndexRepository.java
+++ src/main/java/searchengine/repository/IndexRepository.java
@@ -3,7 +3,6 @@ package searchengine.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import searchengine.model.IndexEntity;
-import searchengine.model.PageEntity;
 
 @Repository
 public interface IndexRepository extends JpaRepository<IndexEntity, Long> {
--- src/main/java/searchengine/repository/LemmaRepository.java
+++ src/main/java/searchengine/repository/LemmaRepository.java
@@ -3,9 +3,6 @@ package searchengine.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import searchengine.model.LemmaEntity;
-import searchengine.model.PageEntity;
-import searchengine.model.SiteEntity;
-
 import java.util.Optional;
 
 @Repository
