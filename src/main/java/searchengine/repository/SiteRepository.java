bbce6ec2d71b175e492b4856da7f3bddd6453a00
Benhap1 <bahtiyar.bobokulov1@gmail.com>
2024-06-02 16:52:01 +0300

    14 commit: deletion of dublicate lemmas in database

--- src/main/java/searchengine/repository/SiteRepository.java
+++ src/main/java/searchengine/repository/SiteRepository.java
@@ -1,27 +1,14 @@
 package searchengine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import searchengine.config.Site;
 import searchengine.model.SiteEntity;
-import searchengine.model.SiteStatus;
-
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SiteRepository extends JpaRepository<SiteEntity, Long> {
 
-    @Modifying
-    @Query("DELETE FROM SiteEntity s WHERE s.url = :url AND s.status = :status")
-    void deleteByUrlAndStatus(@Param("url") String url, @Param("status") SiteStatus status);
-
     Optional<SiteEntity> findByUrl(String url);
-
     Optional<SiteEntity> findByUrlContaining(String host);
-
 }
 
