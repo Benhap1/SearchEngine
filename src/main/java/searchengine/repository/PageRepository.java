ec69444134b8f8b728a442381838026b4998ed22
Benhap1 <bahtiyar.bobokulov1@gmail.com>
2024-06-03 13:04:01 +0300

    16 commit: final optimization of indexing process

--- src/main/java/searchengine/repository/PageRepository.java
+++ src/main/java/searchengine/repository/PageRepository.java
@@ -4,14 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import searchengine.model.PageEntity;
 import searchengine.model.SiteEntity;
-
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PageRepository extends JpaRepository<PageEntity, Long> {
-
-//    List<PageEntity> findBySite(SiteEntity site);
 
     Optional<PageEntity> findBySiteAndPath(SiteEntity siteEntity, String path);
 }
